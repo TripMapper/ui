b3e602559ae372ad8775dc08ca5e5d9effc9015d
Tam <hi@tam.sx>
2022-08-15 14:56:49 +0100

    Add Prose

---
+++ src/Prose/index.tsx
@@ -0,0 +1,21 @@
+import { Children } from 'react';
+import parse from 'html-react-parser';
+
+export default function Prose ({ children }) {
+	return (
+		<>
+			{Children.map(children, child => {
+				if (typeof child === 'string') {
+					const parsed = parse(child);
+
+					if (typeof parsed === 'string')
+						return <p>{parsed}</p>;
+
+					return parsed;
+				}
+
+				return child;
+			})}
+		</>
+	);
+}
