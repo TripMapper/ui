d266e8a260d9c16d13a1266bead7da319549603b
Tam <hi@tam.sx>
2022-08-24 09:30:21 +0100

    Build ui

--- dist/InfoBlock/index.jsx
+++ dist/InfoBlock/index.jsx
@@ -2,12 +2,13 @@ import css from './style.module.scss';
 import Icon from '../Icon';
 import { useState } from 'react';
 import Slideover from '../Slideover';
+import { cx } from '../util';
 const WHITESPACE = <>&nbsp;</>;
 export default function InfoBlock({ icon, primary, secondary, count, label, children, }) {
     const [open, setOpen] = useState(false);
     const onClick = () => setOpen(true), onRequestClose = () => setOpen(false);
     return (<>
-			<button type="button" className={css.infoBlock} onClick={onClick}>
+			<button type="button" className={cx(css.infoBlock, open && css.focus)} onClick={onClick}>
 				<div>
 					{count && <span className={css.count}>{count}</span>}
 					<Icon of={icon} xl/>
