264d1a647a338f17b91f821ec49d45997ab1501b
Tam <hi@tam.sx>
2022-09-08 14:46:22 +0100

    Ensure forms only submit their own content

--- dist/Form/index.jsx
+++ dist/Form/index.jsx
@@ -1,13 +1,19 @@
 import css from './style.module.scss';
+import { useRef } from 'react';
 import formToObj from '../util/formToObj';
 import { cx } from '../util';
 export default function Form({ onSubmit, children, className, ...props }) {
+    const self = useRef();
     const _onSubmit = async (e) => {
         e.preventDefault();
         e.persist();
+        if (e.target !== self.current) {
+            process.env.NODE_ENV === 'development' && console.warn('Tried submitting from another form!', self.current, e.target);
+            return;
+        }
         onSubmit && onSubmit(formToObj(new FormData(e.target), e.target), e);
     };
-    return (<form {...props} onSubmit={_onSubmit} className={cx(css.form, className)}>
+    return (<form {...props} onSubmit={_onSubmit} className={cx(css.form, className)} ref={self}>
 			{children}
 		</form>);
 }
--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,5 +1,5 @@
 import css from './style.module.scss';
-import { FormEvent } from 'react';
+import { FormEvent, useRef } from 'react';
 import formToObj, { FormValue } from '../util/formToObj';
 import { cx } from '../util';
 
@@ -14,14 +14,22 @@ export interface FormProps extends Omit<HTMLFormElement, 'onSubmit'> {
 }
 
 export default function Form ({ onSubmit, children, className, ...props } : FormProps) {
+	const self = useRef();
+
 	const _onSubmit = async e => {
 		e.preventDefault();
 		e.persist();
+
+		if (e.target !== self.current) {
+			process.env.NODE_ENV === 'development' && console.warn('Tried submitting from another form!', self.current, e.target);
+			return;
+		}
+
 		onSubmit && onSubmit(formToObj(new FormData(e.target), e.target), e);
 	};
 
 	return (
-		<form {...props} onSubmit={_onSubmit} className={cx(css.form, className)}>
+		<form {...props} onSubmit={_onSubmit} className={cx(css.form, className)} ref={self}>
 			{children}
 		</form>
 	);
