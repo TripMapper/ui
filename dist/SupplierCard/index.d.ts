1225232e44662196f481cd33e3384b70166e6e74
Tam <hi@tam.sx>
2022-11-14 15:53:25 +0000

    Add supplier card

---
+++ dist/SupplierCard/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+export declare const SUPPLIER_CARD_FRAGMENT: import("urql").TypedDocumentNode<any, object>;
+export default function SupplierCard({ logo, name, email, rating, paid, onPaidClick, onRemoveClick }: {
+    logo: any;
+    name: any;
+    email: any;
+    rating: any;
+    paid: any;
+    onPaidClick: any;
+    onRemoveClick: any;
+}): JSX.Element;
---
+++ dist/SupplierCard/index.jsx
@@ -0,0 +1,44 @@
+import css from './style.module.scss';
+import { gql } from 'urql';
+import Image, { IMAGE_FRAGMENT } from '../Image';
+import { cx } from '../util';
+import RedX from '../svg/red-x.svg';
+export const SUPPLIER_CARD_FRAGMENT = gql `
+	fragment SupplierCard on Supplier {
+		name
+		email: metadataList (
+			condition: { primaryType: EMAIL }
+			first: 1
+		) {
+			value
+		}
+		rating
+		logo {
+			srcset (
+				width: 48
+				height: 48
+			) {
+				...Image
+			}
+        }
+	}
+	${IMAGE_FRAGMENT}
+`;
+const Rating = ({ int }) => (<>
+		{Array.from({ length: 5 }, (_, i) => (<i key={i} className={cx(css.rating, int > i && css.on)}/>))}
+	</>);
+export default function SupplierCard({ logo, name, email, rating, paid, onPaidClick, onRemoveClick }) {
+    return (<div className={css.supplierCard}>
+			{(logo ?? []).length > 0 ? (<Image {...logo[0].srcset} circle className={css.img}/>) : (<span className={css.imgPlaceholder}/>)}
+			<div>
+				<strong>{name} {rating > 0 && <Rating int={rating}/>}</strong>
+				<span>{email?.[0]?.value}</span>
+			</div>
+			{onPaidClick && (<button type="button" className={cx(css.paid, paid && css.isPaid)} onClick={onPaidClick}>
+					{paid ? 'Paid' : 'Unpaid'}
+				</button>)}
+			{onRemoveClick && (<button type="button" onClick={onRemoveClick} title="Remove Supplier" className={css.remove}>
+					<RedX />
+				</button>)}
+		</div>);
+}
